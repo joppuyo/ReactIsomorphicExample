2f420654d460e84c07084453a64620faa65203d2
Johannes Siipola <johannes@siipo.la>
2016-11-19 21:35:14 +0200

    React data loading

--- browser.js
+++ browser.js
@@ -1,24 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Link, browserHistory, IndexRedirect } from 'react-router';
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import axios from 'axios';
 
-class HelloMessage extends React.Component {
-    constructor(props) {
-        console.log(props);
-        super(props)
+import { ReduxAsyncConnect, asyncConnect, reducer as reduxAsyncConnect } from 'redux-connect'
+
+/*
+let counter = (state = {}, action) => {
+    if (action.type === 'GET_DATA') {
+        return {
+            "slug": "widgets",
+            "title": "Widgets",
+            "description": "Leverage agile frameworks to provide a robust synopsis for high level overviews.",
+            "body": "Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment."
+        }
     }
-    render() {
-        return <div>Hello React</div>;
-    }
-}
+    return state;
+};
+
+let store = createStore(counter);
+
+
+store.subscribe(() =>
+    console.log(store.getState())
+);
+*/
 
 class App extends React.Component {
     render() {
         return (
-            <Router history={browserHistory}>
+            <Router render={(props) => <ReduxAsyncConnect {...props}/>} history={browserHistory}>
                 <Route path="/" component={Page}>
                     <IndexRedirect to="widgets" />
-                    <Route path=":pageSlug" component={HelloMessage}/>
+                    <Route path=":pageSlug" component={Content}/>
                 </Route>
             </Router>
         );
@@ -48,4 +64,30 @@ class Page extends React.Component {
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('content'));
+@asyncConnect([{
+    key: 'content',
+    promise: ({ params, helpers }) => {
+        return axios.request(`/api/pages/${params.pageSlug}`).then((response) => {
+            console.log(response);
+            return response.data;
+        });
+    }
+}])
+class Content extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.content.title}</h1>
+                <p>{this.props.content.body}</p>
+            </div>
+        )
+    }
+}
+
+const store = createStore(combineReducers({ reduxAsyncConnect }), window.__data);
+
+ReactDOM.render(
+    <Provider store={store} key="provider">
+        <App />
+    </Provider>, document.getElementById('content')
+);
