c810eaf42242fc564908e277e707365df8c32309
Johannes Siipola <johannes@siipo.la>
2016-11-19 19:29:59 +0200

    Add REST API

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ var fs = require('fs');
 
 app.use('/public', express.static('public'));
 
-app.get('/\*', function (req, res) {
+app.get('/api/pages/:pageId', (request, response) => {
+    fs.readFile('fixtures/' + request.params.pageId + '.json', 'utf-8', (error, json) => {
+        if (error) {
+            response.status(404).send('Not Found');
+        }
+        response.type('json');
+        response.send(json);
+    });
+
+});
+
+app.get('/\*', (req, res) => {
     var template = fs.readFileSync('index.nunjucks', 'utf-8');
     res.send(template);
 });
